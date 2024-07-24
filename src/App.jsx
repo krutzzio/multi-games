4ad9cd1df73b01d062b54f8f697ed64860ac5da5
Krutzzio <gerard.costa.curr@gmail.com>
2024-07-24 17:58:07 +0200

    comit bbf

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <header className='h-24 text-xl'>
           <h1>Choose and <br /><span className='colorRainbow text-transparent animate-gradient text-4xl'>PLAY BIG BANG TECH!</span></h1>
         </header>
-        <main className='w-full h-1/2  flex flex-col justify-center gap-8 text-xl pl-2'>
+        <main className='w-full h-1/2 flex flex-col justify-center gap-8 text-xl pl-2'>
           <h1 className={`${rainBow === "tiktak" ? `colorRainbow text-transparent animate-gradient` : ``} cursor-pointer [word-spacing:-10px]`} onClick={() => setRainBow("tiktak")}>Tik Tak Toe</h1>
           <h1 className={`${rainBow === "sprites" ? `colorRainbow text-transparent animate-gradient` : ``} cursor-pointer`} onClick={() => setRainBow("sprites")} /*onMouseLeave={() => setRainBow("")}*/>Draw Sprites</h1>
           <h1 className={`${rainBow === "memo" ? `colorRainbow text-transparent animate-gradient` : ``} cursor-pointer`} onClick={() => setRainBow("memo")}>Memory</h1>
